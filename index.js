84081b04c12b0c5a371c73738864ec830b960efc
ggm <godgunman@gmail.com>
2017-09-14 21:27:53 +0800

    Add facebook place query

--- index.js
+++ index.js
@@ -31,27 +31,61 @@ app.get('/query-address', function (req, res) {
     })
 })
 
+const getPlaceByGoolge = (location, type) => {
+  let promise = new Promise((resolve, reject) => {
+    let url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    let options = {
+      url: url,
+      qs: {
+        key: 'AIzaSyBjSOxQ59gfngBwu4HMZZS8XrJMmIvt5q0',
+        location: location,
+        radius: '1000',
+        type: type,
+      },
+      method: 'GET'
+    }
+    request(options, function (error, response, body) {
+      if (error) { reject(error) }
+      else { resolve(body) }
+    })
+  })
+  return promise;
+}
+
+const getPlaceByFacebook = (center) => {
+  let promise = new Promise((resolve, reject) => {
+    let url = 'https://graph.facebook.com/v2.10/search'
+    let options = {
+      url: url,
+      qs: {
+        type: 'place',
+        center: center,
+        access_token: 'EAAYqprlhwr8BAPrUuCUIYO83soPiDsjZCb0qOW3xe9FZCrj6I0gtBPD2BgLNPGl0CJkX93HlME2Y9ZC1pU0RGm1eGKGhtqZBTYUgUaYklfD7VBUZAlQVuZB8zJAOwePXCRSDK0F8UNZCFbAUFOky8bH9wveNyZAz08kO87ir5yDJFHjnlq1ZBF1N94KtkT8GP4H6xNZAxWgEvswgZDZD',
+        fields: 'about,name,description,category_list',
+      },
+      method: 'GET'
+    }
+    request(options, function (error, response, body) {
+      if (error) { reject(error) }
+      else { resolve(body) }
+    })
+  })
+  return promise;
+}
+
+
 // /query-place?location=25.019127,121.541980&type=bar
 app.get('/query-place', function (req, res) {
   let location = req.query.location
   let type = req.query.type
-  let url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-  let options = {
-    url: url,
-    qs: {
-      key: 'AIzaSyBjSOxQ59gfngBwu4HMZZS8XrJMmIvt5q0',
-      location: location,
-      radius: '1000',
-      type: type,
-    },
-    method: 'GET'
-  }
-  request(options, function (error, response, body) {
-    console.log('error:', error)
-    console.log('statusCode:', response.statusCode)
-    console.log('body:', body)
-    res.send(body)
-  })
+  let promise1 = getPlaceByGoolge(location, type)
+  let promise2 = getPlaceByFacebook(location)
+  Promise.all([promise1, promise2]).then((result) => {
+    res.send({
+      google: JSON.parse(result[0]),
+      facebook: JSON.parse(result[1]),
+    })
+  });
 })
 
 app.listen(port, function () {
