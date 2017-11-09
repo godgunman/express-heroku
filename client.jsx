e35234f705eab506449f09b15e0c70808de59e76
godgunman <godgunman@gmail.com>
2017-11-09 20:40:09 +0800

    Update

--- client.jsx
+++ client.jsx
@@ -6,20 +6,34 @@ const getApiUrl = () => {
   return `${window.location.protocol}//${window.location.host}`;
 }
 
-const getHistory = async () => {
-  let url = getApiUrl() + '/api/history/';
-  let { data } = await axios.get(url);
-  console.log('data: ', data)
-}
+class History extends React.Component {
 
-class Hello extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    };
+  }
+
+  async getHistory() {
+    let url = getApiUrl() + '/api/history/';
+    let { data } = await axios.get(url);
+    console.log(data);
+    return data;
+  }
+
+  componentDidMount() {
+    this.getHistory();
+  }
+
   render() {
+    // let result = (await this.getHistory())[0];
     return <div>Hello {this.props.name}</div>;
   }
 }
 
 // 將 hello 元件載入 root element 裡頭
 ReactDOM.render(
-  <Hello name="ggm" />,
+  <History />,
   document.getElementById('root')
 )
