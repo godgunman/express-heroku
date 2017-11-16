cdd1c5823e5a9ad2e613a56d1c4144d18cd02333
godgunman <godgunman@gmail.com>
2017-11-16 20:47:57 +0800

    update

--- search.js
+++ search.js
@@ -4,12 +4,12 @@ const getInfo = require("./getInfo")
 const KEY = process.env.KEY || 'AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE'
 
 const searchRestaurant = async address => {
-  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${KEY}`
+  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${KEY}&language=zh-TW`
 
   let addressBody = await rp(addressUrl)
   let { lat, lng, formatted_address } = getInfo(JSON.parse(addressBody))
   let location = `${lat},${lng}`
-  let placeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=${KEY}`
+  let placeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=${KEY}&language=zh-TW`
 
   let placeBody = await rp(placeUrl)
   placeBody = JSON.parse(placeBody)
